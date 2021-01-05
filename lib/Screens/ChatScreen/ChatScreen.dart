aa8f7f8bb87495bbd30265a77c957fc3c422d2cb
Harsh Saini <harshs0203@gmail.com>
2021-01-05 05:37:05 +0530

    Stream setup

--- lib/Screens/ChatScreen/ChatScreen.dart
+++ lib/Screens/ChatScreen/ChatScreen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:kiras_chat/Services/AuthService.dart';
 import 'package:kiras_chat/constants.dart';
@@ -39,30 +40,64 @@ class _ChatScreenState extends State<ChatScreen> {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 10.0),
-          child: Container(
-            decoration: kMessageContainerDecoration,
-            child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    child: TextField(
-                      //controller: messageTextController,
-                      onChanged: (value) {
-                        messageText = value;
-                      },
-                      decoration: kMessageTextFieldDecoration,
-                    ),
-                  ),
-                  FlatButton(
-                    onPressed: () {
-                      _bca.addingDataToBcaFireStore(messageText, _auth.getCurrentUser());
-                    },
-                    child: Text(
-                      'Send',
-                      style: kSendButtonTextStyle,
-                    ),
-                  ),
-                ]),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              StreamBuilder<QuerySnapshot>(
+                  stream: _bca.bcaCollection.snapshots(),
+                  // ignore: missing_return
+                  builder: (context, snapshot){
+                    if(!snapshot.hasData) {
+                      return Center(
+                        child: CircularProgressIndicator(
+                          backgroundColor: Colors.blue[800],
+                        ),
+                      );
+                    }
+                      final messages = snapshot.data.docs;
+                      List<Text> messageWidgets = [];
+                      for (var message in messages) {
+                        //final messagesText = message.data().containsKey('text');
+                        final messagesText = message.data()['text'];
+                        final messagesSender = message.data()['sender'];
+                        final messageWidget = Text(
+                            '$messagesText from $messagesSender');
+                        messageWidgets.add(messageWidget);
+                      }
+                      return Expanded(
+                        child: ListView(
+                          padding: EdgeInsets.symmetric(horizontal: 10.0,vertical: 20.0),
+                          children: messageWidgets,
+                        ),
+                      );
+                  }),
+              Container(
+                decoration: kMessageContainerDecoration,
+                child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                        child: TextField(
+                          //controller: messageTextController,
+                          onChanged: (value) {
+                            messageText = value;
+                          },
+                          decoration: kMessageTextFieldDecoration,
+                        ),
+                      ),
+                      FlatButton(
+                        onPressed: () {
+                          _bca.addingDataToBcaFireStore(messageText, _auth.getCurrentUser());
+                        },
+                        child: Text(
+                          'Send',
+                          style: kSendButtonTextStyle,
+                        ),
+                      ),
+                    ]),
+              ),
+            ],
           ),
         ),
       ),
